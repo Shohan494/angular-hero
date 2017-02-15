1c45cfed5bf86e3ad67b6a88c7cc54a0607ac4e4
shohan494 <shohan12224943@gmail.com>
2017-02-16 01:03:03 +0600

    learned to looping and adding css in angular way, adding logic and onclick event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,111 @@
 import { Component } from '@angular/core';
 
+//single hero class with properties
 export class Hero {
   id: number;
-  title: string;
   name: string;
 }
 
+// declaring the array of some heroes
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
-  //with an input, one way binding, means the value change does not reflect anywhere else
   selector: 'my-app',
-  // the view, data will be shown here
-  template:`
+  template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <!--update the template to use the ngModel built-in directive for two-way binding.-->
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+    <!-- looping through with ("*ngFor") and "onclick event" added on "hero"-->
+    <!-- a class will be added when a single hero is selected on clicking -->
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <!-- selected hero template part -->
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
-    `})
+  `,
+  //style css section
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
+})
 
 // the class of app component, data will be passsed from here
 export class AppComponent{
   //can also be used from here
   title = "Tour Of Heroes";
-  //name = 'Windstorm';
+  //arraay of some heroes
+  heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    title: "Tour of Heroes",
-    name: 'Windstorm The Saviour'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  }
 }
